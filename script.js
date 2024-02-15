2c6e226f8fc865260b6e2980f52cb1a2021dd20c
codeglimpse <113093370+dhonaobina@users.noreply.github.com>
2024-02-15 20:10:02 +1300

    added Number, Boolean and String - datatypes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var x = 3.14;
 var y = 3;
 document.getElementById('number').innerHTML = "Value of x = " + x + "<br>" + "Value of y = " + y;
+
+var a = 9;
+var b = 10;
+document.getElementById('Boolean').innerHTML = "If the value of a is greater than the value of b, the Boolean variable will return a true value. Else, it will return a false value,";
+
+var c = "I";
+var d = 'like mangoes.';
+document.getElementById('string').innerHTML = c + " " + d;
