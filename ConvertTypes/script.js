c9c8177d4ece630603d1c0171382beeed4053943
codeglimpse <113093370+dhonaobina@users.noreply.github.com>
2024-02-16 18:52:47 +1300

    added strict and conversion

--- ConvertTypes/script.js
+++ ConvertTypes/script.js
@@ -1,3 +1,5 @@
+'use script';
+
 /* CONVERT TYPES */
 document.getElementById("typeofdemo").innerHTML =
 'Data type of "Apples" is ' + typeof "Apples" + "<br>" 
@@ -5,4 +7,10 @@ document.getElementById("typeofdemo").innerHTML =
 + 'Data type of 7 is ' + typeof 7 +  "<br>" 
 + 'Data type of false is ' + typeof false + "<br>" 
 + 'Data type of an undeclared variable is   ' + typeof myvar + "<br>" + 'Data type of null is ' + typeof null;
+
+x = 6.44;
+document.getElementById('strict').innerHTML = "Value of x = " + x;
+
+var z = 121;
+document.getElementById('conversion').innerHTML = "The data type of z before conversion is " + typeof z + "<br>" + "The data type after conversion is " + typeof String(z);
 
