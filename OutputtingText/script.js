92dd445cb4d669529641a445473ae084ccb553fe
codeglimpse <113093370+dhonaobina@users.noreply.github.com>
2024-02-17 07:36:25 +1300

    output text method done

---
+++ OutputtingText/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.getElementById('outputval').innerHTML = 'I am learning JavaScript';
+
+/* This code doesn't work on external file */
+document.write('Outputting text using document.write() method...');
