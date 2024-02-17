f7e8eded96af53ab9681d5e39e0af64aeca5318c
codeglimpse <113093370+dhonaobina@users.noreply.github.com>
2024-02-18 07:49:50 +1300

    function with parameters done

--- functionsWithParameters/script.js
+++ functionsWithParameters/script.js
@@ -1,15 +1,24 @@
+/*callf1*/
 function mystrfun(a) 
 {
     var b = 'The string that was passed to this function is: ' + a;
     return b;
 }
 
-document.getElementById("callf1").innerHTML = mystrfun('Planet Earth');
-
+/*callf2*/
 function other_mystrfun(c)
 {
     var d = 'I want to learn to code and  ' + c;
     return d;
 }
 
+/*callf3*/
+function mycalc (num1, num2) 
+{
+    var val = num1 + ` multiplied by `+ num2 + `  is: ` + num1*num2;
+    return val;
+}
+
+document.getElementById("callf1").innerHTML = mystrfun('Planet Earth');
 document.getElementById("callf2").innerHTML = other_mystrfun('I will not give up!');
+document.getElementById("callf3").innerHTML = mycalc(32,54);
